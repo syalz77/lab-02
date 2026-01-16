5a5f466f5a2f03fef0ba32f679358d3238ad2777
Ali Zaidi <syedalzd772@gmail.com>
2026-01-15 20:20:17 -0700

    Worked on ListyCity

---
+++ app/src/main/java/com/example/listycity/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.listycity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    // Declare the variables so that you will be able to reference it later
+    ListView cityList;
+    ArrayAdapter<String> cityAdapter;
+    ArrayList<String> dataList;
+    int cityPosition = -1;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cityList = findViewById(R.id.city_list);
+
+        String []cities = {"Edmonton", "Montreal"};
+
+        dataList = new ArrayList<>();
+        dataList.addAll(Arrays.asList(cities));
+
+        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+        cityList.setAdapter(cityAdapter);
+
+        Button addButton = findViewById(R.id.add_button);
+        Button deleteButton = findViewById(R.id.delete_button);
+        EditText inputField = findViewById(R.id.input_field);
+        Button confirmButton = findViewById(R.id.confirm_button);
+
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inputField.setVisibility(View.VISIBLE);
+                confirmButton.setVisibility(View.VISIBLE);
+            }
+        });
+
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String city = inputField.getText().toString();
+                dataList.add(city);
+                cityAdapter.notifyDataSetChanged();
+                inputField.setText("");
+                inputField.setVisibility(View.GONE);
+                confirmButton.setVisibility(View.GONE);
+            }
+        });
+
+        cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                cityPosition = position;
+
+            }
+        });
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (cityPosition != -1) {
+                    dataList.remove(cityPosition);
+                    cityAdapter.notifyDataSetChanged();
+                    cityPosition= -1;
+                }
+            }
+        });
+
+
+
+
+
+        EdgeToEdge.enable(this);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
